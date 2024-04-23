a15af6751c5d4bddaefb503e7a3914f2def11561
Shoxrux <davlatyorxudayarov@gmail.com>
2024-04-23 21:46:19 +0500

    fixed login lowercase bug

--- api/views.py
+++ api/views.py
@@ -116,6 +116,8 @@ class CreateUsedServicesView(CreateAPIView):
 class LoginViewAPI(APIView):
 
     def get(self, request, username, password):
+        username = username.lower()
+        print(username)
         user = get_object_or_404(User, username=username)
         if LoginSystem.objects.filter(username=user.id, password=password):
             context = {
